515dcadf58a7d04381acb5954e158059563bd00d
Sunny NG <ngsanluk@gmail.com>
2017-02-28 01:18:33 +0800

    Initial commit

--- MapViewDemo/ViewController.swift
+++ MapViewDemo/ViewController.swift
@@ -7,12 +7,18 @@
 //
 
 import UIKit
+import MapKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, MKMapViewDelegate{
 
+    @IBOutlet weak var mapView: MKMapView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        self.mapView.showsUserLocation = true
+        self.mapView.delegate = self
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +26,51 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func changeMapType(_ sender: Any) {
+        if self.mapView.mapType == MKMapType.standard {
+            self.mapView.mapType = MKMapType.satellite
+        }
+        else if self.mapView.mapType == MKMapType.satellite {
+            self.mapView.mapType = MKMapType.hybrid
+        }
+        else {
+            self.mapView.mapType = MKMapType.standard
+        }
+    }
+    
+    @IBAction func zoomIn(_ sender: Any) {
+        let userLocation = mapView.userLocation
+        // Defining a smaller targeted span means zooming in
+        let newSpan = MKCoordinateSpanMake(
+            mapView.region.span.latitudeDelta * 0.5,
+                        mapView.region.span.longitudeDelta * 0.5)
+        let newRegion = MKCoordinateRegion(
+            center: userLocation.location!.coordinate,
+            span: newSpan
+        )
+        mapView.setRegion(newRegion, animated: true)
+    }
+    
+    
+    @IBAction func zoomOut(_ sender: Any) {
+        let userLocation = mapView.userLocation
+        // Defining a bigger targeted span means zooming out
+        let newSpan = MKCoordinateSpanMake(
+            mapView.region.span.latitudeDelta * 2,
+            mapView.region.span.longitudeDelta * 2)
+        let newRegion = MKCoordinateRegion(
+            center: userLocation.location!.coordinate,
+            span: newSpan
+        )
+        mapView.setRegion(newRegion, animated: true)
+    
+    }
+    
+    
+    func mapView(_ mapView: MKMapView, didUpdate userLocation: MKUserLocation) {
+        self.mapView.centerCoordinate = userLocation.location!.coordinate
+    }
+    
+    
 }
 
---
+++ README.md
@@ -0,0 +1,2 @@
+# Simple MKMapView Demo
+In this simple MKMapView demo app, you can change map type (from standard to satellite to hybrid) and do zooming in/out to map view. 
